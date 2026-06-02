2d93329e85b97821483cf940c64e15f35298dbc3
Benjamin Michel <144721210+Benix1107@users.noreply.github.com>
2026-06-02 12:01:03 +0200

    Reorder accounts array in bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ process.on('unhandledRejection', (reason) => {
 });
 
 const accounts = [
-    'FreeB4B', 'NebelBenix', 'alkaner12', 'Dihloco', 'meloupup', 'DrDihNut', 'Aawaz3', 'Saugroboter3', 'Oafka40', 'FullestFox'
+    'NebelBand', 'FreeB4B', 'NebelBenix', 'alkaner12', 'Dihloco', 'meloupup', 'DrDihNut', 'Aawaz3', 'Saugroboter3', 'Oafka40', 'FullestFox'
 ];
 
 const proxies = [
